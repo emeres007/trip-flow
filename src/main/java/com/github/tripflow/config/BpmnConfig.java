6efa9afd05a2816984b1199c92a04212ce3b76be
emeres <mic.skwarek@gmail.com>
2026-02-07 20:09:52 +0100

    values-local.yaml

--- src/main/java/com/github/tripflow/config/BpmnConfig.java
+++ src/main/java/com/github/tripflow/config/BpmnConfig.java
@@ -1,20 +1,25 @@
 package com.github.tripflow.config;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class BpmnConfig {
 
     @Bean
     public CommandLineRunner deployWorkflows(ZeebeClient client) {
+        log.info("Deploying BPMN process - trip-flow...");
         return args -> {
-            client.newDeployResourceCommand()
+            var event = client.newDeployResourceCommand()
                     .addResourceFromClasspath("bpmn/trip-flow.bpmn")
                     .send()
                     .join();
+
+            log.info("Deploy result: {}", event);
         };
     }
 }
